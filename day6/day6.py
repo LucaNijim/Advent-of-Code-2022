ef37ccc5be022d75b949919a92d7335b7e25aa42
LucaNijim <luca.nijim@gmail.com>
2022-12-07 20:33:53 -0500

    Day 6 done!

---
+++ day6/day6.py
@@ -0,0 +1,14 @@
+input_string: str = open('day6input.txt', 'r').readline()
+
+
+def locate_packet(string, n):
+    index = 0
+    while index < len(input_string):
+        potential_packet = set(x for x in string[index:index + n])
+        if len(potential_packet) == n:
+            break
+        index += 1
+    return index + n
+
+
+print(str(locate_packet(input_string, 4)) + '\n' + str(locate_packet(input_string, 14)))
