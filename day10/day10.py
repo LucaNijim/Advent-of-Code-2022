e7eb03aaf1fb56a197ec41a70649c0a3fdcb4097
LucaNijim <luca.nijim@gmail.com>
2022-12-10 15:32:37 -0500

    Day 10 done. This one was super cool!

---
+++ day10/day10.py
@@ -0,0 +1,37 @@
+class CathodeRayTube:
+    def __init__(self):
+        self.cycle = 0
+        self.x = 1
+        self.signal = 0
+        self.screen = []
+
+    def noop(self):
+        if self.cycle % 40 == 0:
+            self.screen.append('')
+        self.screen[-1] += '#' if self.x-1 <= self.cycle%40 <= self.x+1 else ' '
+        self.cycle += 1
+        self.signal += self.cycle * self.x if self.cycle % 40 == 20 else 0
+
+    def addx(self, val):
+        self.noop()
+        self.noop()
+        self.x += val
+
+    def execute(self, file):
+        file_unpack = open(file).readlines()
+        for line in file_unpack:
+            command = line.strip(' \n').split()
+            if command[0] == 'noop':
+                self.noop()
+            elif command[0] == 'addx':
+                self.addx(int(command[1]))
+
+
+CRT = CathodeRayTube()
+CRT.execute('day10input.txt')
+print(CRT.signal)
+screen_string = ''
+for line in CRT.screen:
+    screen_string += line
+    screen_string += '\n'
+print(screen_string)
