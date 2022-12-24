61b492fb28bf8c11f889d6a3b76d78a74b059d92
LucaNijim <luca.nijim@gmail.com>
2022-12-23 21:18:05 -0500

    Day 21 cleaned up

--- day21/day21.py
+++ day21/day21.py
@@ -1,4 +1,14 @@
 from math import floor
+from time import time
+
+
+def timeit(func):
+    def helper(*args, **kwargs):
+        start_time = time()
+        func(*args, **kwargs)
+        print('Time elapsed: '+ str(time()-start_time))
+
+    return helper
 
 
 class MonkeyGroup:
@@ -7,39 +17,38 @@ class MonkeyGroup:
         for line in open(file).readlines():
             self.monkeys[line[0:4]] = line.split()[1:]
 
-        def eval_monkey(monkey):
-            if isinstance(self.monkeys[monkey], int):
-                return self.monkeys[monkey]
-            if len(self.monkeys[monkey]) == 1:
-                self.monkeys[monkey] = int(self.monkeys[monkey][0])
-                return self.monkeys[monkey]
-            if self.monkeys[monkey][1] == '+':
-                return eval_monkey(self.monkeys[monkey][0]) + eval_monkey(self.monkeys[monkey][2])
-            elif self.monkeys[monkey][1] == '-':
-                return eval_monkey(self.monkeys[monkey][0]) - eval_monkey(self.monkeys[monkey][2])
-            elif self.monkeys[monkey][1] == '*':
-                return eval_monkey(self.monkeys[monkey][0]) * eval_monkey(self.monkeys[monkey][2])
-            elif self.monkeys[monkey][1] == '/':
-                return int(eval_monkey(self.monkeys[monkey][0]) / eval_monkey(self.monkeys[monkey][2]))
+    def eval_monkey(self, monkey):
+        if isinstance(self.monkeys[monkey], int):
+            return self.monkeys[monkey]
+        if len(self.monkeys[monkey]) == 1:
+            self.monkeys[monkey] = int(self.monkeys[monkey][0])
+            return self.monkeys[monkey]
+        if self.monkeys[monkey][1] == '+':
+            return self.eval_monkey(self.monkeys[monkey][0]) + self.eval_monkey(self.monkeys[monkey][2])
+        elif self.monkeys[monkey][1] == '-':
+            return self.eval_monkey(self.monkeys[monkey][0]) - self.eval_monkey(self.monkeys[monkey][2])
+        elif self.monkeys[monkey][1] == '*':
+            return self.eval_monkey(self.monkeys[monkey][0]) * self.eval_monkey(self.monkeys[monkey][2])
+        elif self.monkeys[monkey][1] == '/':
+            return int(self.eval_monkey(self.monkeys[monkey][0]) / self.eval_monkey(self.monkeys[monkey][2]))
 
-        print('Part 1 result: ' + str(eval_monkey('root')))
-
-        def binary_search(dictionary, low, high, comp_val):
-            mid = floor((low + high) / 2)
-            dictionary['humn'] = mid
-            val = eval_monkey(dictionary['root'][0])
-            if val == comp_val:
-                return mid
-            if val < comp_val:
-                return binary_search(dictionary, low, mid, comp_val)
-            if val > comp_val:
-                return binary_search(dictionary, mid, high, comp_val)
-
-        print('Part 2 result: '+str(binary_search(self.monkeys, 1, 10 ** 15, eval_monkey(self.monkeys['root'][2]))))
+    def binary_search(self, low, high, comp_val):
+        mid = floor((low + high) / 2)
+        self.monkeys['humn'] = mid
+        val = self.eval_monkey(self.monkeys['root'][0])
+        if val == comp_val:
+            return mid
+        if val < comp_val:
+            return self.binary_search(low, mid, comp_val)
+        if val > comp_val:
+            return self.binary_search(mid, high, comp_val)
 
 
 def main():
     our_group = MonkeyGroup('day21input.txt')
+    print('Part 1 result: ' + str(timeit(our_group.eval_monkey)('root')))
+    print('Part 2 result: ' + str(timeit(our_group.binary_search)(1, 10 ** 15,
+                                                          our_group.eval_monkey(our_group.monkeys['root'][2]))))
 
 
 if __name__ == '__main__':
