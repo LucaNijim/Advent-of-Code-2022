18421a1ebf245689ad06a3d4fa789801cf708533
LucaNijim <luca.nijim@gmail.com>
2023-01-05 21:28:26 -0500

    Day 19 reformatted

--- day20/day20.py
+++ day20/day20.py
@@ -1,16 +1,19 @@
 def main():
-    with open('day20practiceinput.txt') as file:
-        list_of_numbers = [int(x.strip('\n ')) for x in file.readlines()]
-    length = len(list_of_numbers)
-    list_of_indexes = list(range(length))
-    print(list_of_numbers)
-    print(list_of_indexes, '\n'*2)
+    dec_key = 811589153
+    with open('day20input.txt') as file:
+        original_numbers = [int(x.strip(' \n'))*dec_key for x in file.readlines()]
+    reorder_nums = list(enumerate(original_numbers.copy()))
+    length = len(original_numbers)
 
-    # Let's write out what's happening here.
+    for i in range(10):
+        for index, val in enumerate(original_numbers):
+            j_index = reorder_nums.index((index, val))
+            j = reorder_nums.pop(reorder_nums.index((index, val)))
+            new_j_index = (j[1] + j_index) % (length - 1)
+            reorder_nums.insert(new_j_index, (index, val))
+    reorder_nums = [x[1] for x in reorder_nums]
 
-
-
-    # this wraps the list
+    print(sum([reorder_nums[x] for x in [(y+reorder_nums.index(0)) % length for y in [1000, 2000, 3000]]]))
 
 
 if __name__ == '__main__':
